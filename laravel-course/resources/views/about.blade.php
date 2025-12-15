b76b2739bfa0f36ebd4bda3c6f9570f31c3ae20d
Mojtaba zamani <memojtabazamani@gmail.com>
2025-12-15 12:55:54 +0330

    laravel course

---
+++ laravel-course/resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<h1>
+    Hello world
+</h1>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum ducimus expedita facere facilis in laudantium maiores, nisi quos! A commodi impedit inventore libero nulla. Nisi quam sed tempora voluptatem voluptatum!
+</p>
