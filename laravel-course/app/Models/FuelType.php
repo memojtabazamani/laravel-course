7918e87876ee8447fb7de475b5e7fb0648feff85
Mojtaba zamani <memojtabazamani@gmail.com>
2025-12-18 12:03:11 +0330

    .

---
+++ laravel-course/app/Models/FuelType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FuelType extends Model
+{
+    use HasFactory; // fuel_types
+    // protected $table = 'fuel_types';
+    // protected $primaryKey = 'id';
+    // public $incrementing = false;
+    // protected $keyType = 'string';
+
+//    const CREATED_AT = 'created_at';
+
+//    const UPDATED_AT = 'updated_at';
+    public $timestamps = false;
+}
