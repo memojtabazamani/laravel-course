b5de1dad23c7058f231a5e7200d03270fe787ec2
Mojtaba zamani <memojtabazamani@gmail.com>
2025-12-18 12:03:04 +0330

    laravel-course

--- laravel-course/app/Http/Controllers/HomeController.php
+++ laravel-course/app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Car;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 
@@ -9,6 +10,18 @@ class HomeController extends Controller
 {
     public function index()
     {
+        // Select all cars
+        $cars = Car::all();
+        // Select published cards
+        $cars = Car::where('published_at', '!=', null)->get();
+
+        // Select the first car
+//        $car = Car::where('published_at', '!=', null)->first();
+//        dd($car);
+
+//        $car = Car::find(2);
+        $car = Car::orderBy('created_at', 'desc')->get();
+        dd($car);
         return view('home.index');
     }
 }
